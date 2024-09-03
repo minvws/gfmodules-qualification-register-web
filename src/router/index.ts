9c04b77878d94297b42d7f88a48badd8e57918ba
Gerbrand Bosch <gerbrandb@gmail.com>
2024-09-03 09:57:51 +0200

    Updated default redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      redirect: { name: 'qualifications' }
+      redirect: { name: 'vendor-qualifications' }
     },
     {
       path: '/about',
